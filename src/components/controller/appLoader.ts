acf7a1f47daac3de6684ee9cb699cab0f191b4a8
Nexuslolz <a.klyosov@inbox.ru>
2022-12-13 02:04:11 +0500

    refactor: translate js file to ts

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,10 +2,11 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.up.railway.app/', {
             apiKey: '5d03d150b5e5435ba880e07e934dd012', // получите свой ключ https://newsapi.org/
         });
     }
 }
 
 export default AppLoader;
+//https://newsapi.org/v2/
