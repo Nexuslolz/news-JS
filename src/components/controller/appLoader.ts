9d42f469cce1450b296c3d59d8745c2c24265886
Nexuslolz <a.klyosov@inbox.ru>
2022-12-13 21:31:12 +0500

    refactor: add enum

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -1,9 +1,14 @@
 import Loader from './loader';
 
+enum UrlConstruct {
+    url = 'https://nodenews.up.railway.app/',
+    apiKey = '5d03d150b5e5435ba880e07e934dd012',
+}
+
 class AppLoader extends Loader {
     constructor() {
-        super('https://nodenews.up.railway.app/', {
-            apiKey: '5d03d150b5e5435ba880e07e934dd012', // получите свой ключ https://newsapi.org/
+        super(UrlConstruct.url, {
+            apiKey: UrlConstruct.apiKey, // получите свой ключ https://newsapi.org/
         });
     }
 }
